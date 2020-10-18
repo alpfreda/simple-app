ada475790779a9bf55208e3b691fd57b2a78f0f2
jsgeekes <jsgeekes@gmail.com>
2020-10-18 20:12:01 +0300

    Fix sorting of viewved users & limitation

--- src/redux/reducers/users-reducer.js
+++ src/redux/reducers/users-reducer.js
@@ -4,9 +4,26 @@ import initialState from './initial-state'
 const postsReducer = (state = initialState.users, action) => {
   switch (action.type) {
     case usersConstants.SET_VIEW_USER:
+      const existViewedUsers = Object.assign([], state.viewedUsers)
+      if (existViewedUsers.some(user => user.id === action.payload.id)) {
+        existViewedUsers.map(user =>
+          user.id === action.payload.id
+            ? Object.assign({}, user, {
+                lastUpdateTime: new Date(),
+              })
+            : user,
+        )
+      } else {
+        existViewedUsers.push(
+          Object.assign({}, action.payload, {
+            lastUpdateTime: new Date(),
+          }),
+        )
+      }
+
       return {
         ...state,
-        viewedUsers: [...state.viewedUsers, action.payload],
+        viewedUsers: existViewedUsers.sort((a, b) => b.lastUpdateTime - a.lastUpdateTime).slice(0, 5),
       }
 
     default:
