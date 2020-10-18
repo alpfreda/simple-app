44b73ebdc31ac0df164462961d24876427ccd62b
jsgeekes <jsgeekes@gmail.com>
2020-10-18 15:25:38 +0300

    Fix imported css files

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import PublicLayout from './layouts/public-layout'
 
-import './assets/base.scss'
-
 const App = () => {
   return (
     <Router>
